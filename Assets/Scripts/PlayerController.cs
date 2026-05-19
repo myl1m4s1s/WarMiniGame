61317d9e225bc523d75e8f2843acce2e867d2645
myl1m4s1s <165166043+myl1m4s1s@users.noreply.github.com>
2026-05-19 18:09:38 +0200

    Player animations +Interactable issue
    Added player animations (idle + walking)
    Ran into issues with interactables; window, radio, mailbox. Others working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,35 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private float moveInput;
     private Interactable currentInteractable;
+    private Animator animator;
+    private float originalYScale;
+    private float originalZScale;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        originalYScale = transform.localScale.y;
+        originalZScale = transform.localScale.z;
     }
     
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
+        
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(moveInput));
+        }
+        
+        if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(-1, originalYScale, originalZScale);
+        }
+        else if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(1, originalYScale, originalZScale);
+        }
         
         if (Input.GetKeyDown(KeyCode.W) && currentInteractable != null)
         {
